4a98a364c616227bc693d1893c84909b7e69a4f2
Blade Boles <bladerboles@gmail.com>
2020-03-28 17:44:30 -0600

    First commit

---
+++ src/Main.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class Main extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Dogs vs Cats....vs Cage
+        </h1>
+        <h2>
+          Who is cuter!?
+        </h2>
+      </div>
+    )
+  }
+}
+
+
